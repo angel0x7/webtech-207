6592ae58c9ec042dde056c2c49a9dd92b72c21d9
Angel <angel.velasco0x7@gmail.com>
2025-10-04 12:09:37 +0200

    fit: client web pages

---
+++ lab3/client/src/components/Footer.js
@@ -0,0 +1,15 @@
+
+export default function Footer() {
+  return (
+    <footer
+      style={{
+        background: "#161515ff",
+        padding: "1rem",
+        textAlign: "center",
+        color: "white"
+      }}
+    >
+      <p>© 2025 Ma Web App — Tous droits réservés</p>
+    </footer>
+  );
+}
