08d065ae01ccd52169f09e4c441ce584704e7c91
Mike <michael.adda92@gmail.com>
2025-10-08 10:36:47 +0200

    change: changed  head title

--- threat-map/src/app/components/header.tsx
+++ threat-map/src/app/components/header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header style={{ background: "#000000ff", color: "#fff", padding: "1rem" }}>
       <h1 style={{ margin: 0, textAlign: "center", fontSize: "2rem" }}>
-        Cyber News
+        Threat Map
       </h1>
       <nav style={{ display: "flex", gap: "1rem", background:"#2e2d2dff", padding:"10px" }}>
         <Link href="/"><span style={{ color: "#93d2f0ff" }}>Home</span></Link>
