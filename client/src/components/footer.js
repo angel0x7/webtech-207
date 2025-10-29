138d0d5278da249fd84de8efa2ab257ac04e6e5c
MichaelAdda7 <119415626+MichaelAdda7@users.noreply.github.com>
2025-10-29 09:28:23 +0100

    Delete client directory

--- client/src/components/footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer style={{ textAlign: "center", padding: "1rem", marginTop: "2rem", background: "#f2f2f2" }}>
-      <p>© {new Date().getFullYear()} My Blog. All rights reserved.</p>
-    </footer>
-  );
-}
